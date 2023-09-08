8b7907d6152096da5daa9b94e726c7f585d6d628
faithadewuyi <128841209+faithadewuyi@users.noreply.github.com>
2023-09-08 13:10:14 +0100

    Add files via upload
    Updated

--- script.js
+++ script.js
@@ -5,19 +5,6 @@ const currentDayOfWeek = daysOfWeek[currentDayIndex];
 document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDayOfWeek;
 
 //Current  Time
-function updateTime() {
-    const present = new Date();
-    const hours = String(present.getHours()).padStart(2, '0');
-    const minutes = String(present.getMinutes()).padStart(2, '0');
-    const seconds = String(present.getSeconds()).padStart(2, '0');
-
-    document.getElementById('hours').textContent = hours; // Get hour
-    document.getElementById('minutes').textContent = minutes; // Get minutes
-    document.getElementById('seconds').textContent = seconds; // Get seconds
-}
-
-// Update the clock every second
-setInterval(updateTime, 1000);
-
-// Initial update
-updateTime();
+const currentUTCTime = new Date().getTime();
+        
+ document.getElementById('utcTime').textContent = `Current UTC Time in Milliseconds: ${currentUTCTime}`;
